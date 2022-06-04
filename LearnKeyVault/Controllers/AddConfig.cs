251b1b02cd6d8a1fb56ed35ea8acf41e02bc3cf6
katak <katak@SANATH>
2022-06-04 23:06:54 +0530

    Add project files.

---
+++ LearnKeyVault/Controllers/AddConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace LearnKeyVault.Controllers
+{
+    public class AddConfig
+    {
+        private readonly IConfiguration configuration;
+            public AddConfig(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+                
+        }
+        public string StorrageAccontName { get { return this.configuration["StorrageAccontName"]; } }
+        public string Key { get { return this.configuration["Key"]; } }
+
+        public string Containername { get { return this.configuration["Containername"]; } }
+    }
+}
---
+++ LearnKeyVault/Controllers/FileUploadController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnKeyVault.Controllers
+{
+
+    public class FileUploadController : Controller
+    {
+        private readonly AddConfig config;
+        public FileUploadController(AddConfig config)
+        {
+            this.config = config;
+
+        }
+       
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            var filePaths = new List<string>();
+            foreach (var formfile in files)
+            {
+                filePaths.Add(formfile.FileName);
+                await UploadBlob(formfile.OpenReadStream(), formfile.FileName);
+            }
+
+
+            return Ok(new { message = "File Upload", count = files.Count });
+        }
+
+        public async Task<bool> UploadBlob(Stream filestream, string filename)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(config.Key);
+
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(config.Containername);
+
+            BlobClient blobClient = containerClient.GetBlobClient(filename);
+            await blobClient.UploadAsync(filestream, true);
+
+            return await Task.FromResult(true);
+        }
+
+    }
+}
